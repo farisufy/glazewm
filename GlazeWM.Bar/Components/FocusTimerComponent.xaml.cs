234a096dfe2bc2e1cac2b39baf051e77c8a78079
ImFaris <92919897+farisufy@users.noreply.github.com>
2023-12-30 19:19:13 +0300

    Revert "Revert "Added a simple focus timer component""
    This reverts commit 5628081561861bbe11e891d190d737ca383aad06.

---
+++ GlazeWM.Bar/Components/FocusTimerComponent.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace GlazeWM.Bar.Components
+{
+  /// <summary>
+  /// Interaction logic for FocusTimerComponent.xaml
+  /// </summary>
+  public partial class FocusTimerComponent : UserControl
+  {
+    public FocusTimerComponent()
+    {
+      InitializeComponent();
+    }
+  }
+}
